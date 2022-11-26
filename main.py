b728a810a5755905fe09e7fbfb46ba0b9e0bf725
Cami <camilozume@gmail.com>
2022-11-25 20:11:42 -0500

    Commit apigate

---
+++ main.py
@@ -0,0 +1,62 @@
+from datetime import timedelta
+from flask import Flask, request
+from flask_cors import CORS
+from flask_jwt_extended import (JWTManager, create_access_token, verify_jwt_in_request, get_jwt_identity)
+import requests
+
+from waitress import serve
+import utils
+
+import json
+
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "camilozume"
+cors = CORS(app)
+jwt = JWTManager(app)
+
+
+@app.before_request
+def before_request_callback() -> tuple:
+    endpoint = utils.clear_url(request.path)
+    exclude_routes = ['/login', '/']
+    if exclude_routes.__contains__(request.path):
+        pass
+    elif verify_jwt_in_request():
+        user = get_jwt_identity()
+        if user.get('rol'):
+            has_grant = utils.validate_grant(endpoint, request.method, user['rol'].get('idRol'))
+            if not has_grant:
+                return {"message": "Permission denied"}, 401
+        else:
+            return {"message": "Permission denied"}, 401
+
+
+@app.route("/", methods=['GET'])
+def home():
+    response = {"message": "Hola"}
+    return response
+
+@app.route("/login", methods=['POST'])
+def login() -> tuple:
+    user = request.get_json()
+    url = data_config.get("url-backend-security") + "/user/login"
+    response = requests.post(url, headers=utils.HEADERS, json=user)
+    if  response.status_code==200:
+        user_logged = response.json()
+        expires = timedelta(days=1)
+        access_token = create_access_token(identity=user, expires_delta=expires)
+        return {"token": access_token, "user_id": user_logged.get('id')}, 200
+    else:
+        return {"message": "Access denied"}, 401
+
+# Config and execute app
+
+
+
+if __name__ == '__main__':
+    data_config = utils.load_file_config()
+    print("http://" + data_config.get("url-api-gateway") + ":" + str(data_config.get("port")))
+    serve(app, host=data_config.get("url-api-gateway"), port=data_config.get("port"))
+
+
+
